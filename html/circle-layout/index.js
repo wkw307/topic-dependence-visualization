c8c3be6db65669e892f5678868dc0adc81663836
wkw307 <wkw307@gmail.com>
2019-11-12 10:59:54 +0800

    rm .DS_Store

--- html/circle-layout/index.js
+++ html/circle-layout/index.js
@@ -25,26 +25,48 @@ var link = d3.line()
   .x(function(d){return d.x})
   .y(function(d){return d.y})
   .curve(d3.curveCatmullRom.alpha(0.5));
-var topics = [ '104798', '104799', '104802', '104804', '104805', '104806', '104807', '104809', '104826', '104827' ];
-var links = { '104797': undefined,
-'104798': [ '104827' ],
-'104799': undefined,
-'104802':
- [ '104806',
-   '104798',
-   '104809',
-   '104797',
-   '104799',
-   '104807',
-   '104804',
-   '104805' ],
-'104804': undefined,
-'104805': undefined,
-'104806': [ '104827' ],
-'104807': undefined,
-'104809': undefined,
-'104826': undefined,
-'104827': [ '104826' ] };
+
+var topics = [
+  '104814', '104837',
+  '104841', '104842',
+  '104843'
+];
+var links = {
+  '104814': undefined,
+  '104837': [ '104841', '104814', '104843' ],
+  '104841': [ '104842', '104843' ],
+  '104842': undefined,
+  '104843': undefined
+};
+
+var topics0 = [
+  '104794', '104796',
+  '104801', '104812',
+  '104814', '104817',
+  '104833', '104837',
+  '104841', '104843',
+  '104842'
+];
+var links0 = {
+  '104794': undefined,
+  '104796': undefined,
+  '104801': undefined,
+  '104812': undefined,
+  '104814': [ '104817' ],
+  '104817': undefined,
+  '104833': undefined,
+  '104837': [ '104841', '104794', '104814', '104843' ],
+  '104841': [ '104842', '104833', '104812', '104796', '104843' ],
+  '104842': [ '104801' ],
+  '104843': undefined
+};
+
+var topics2 = [
+  '104814', '104817',
+  '104837','104794',
+  '104841','104796','104812','104833',
+  '104842','104801', '104843'
+];
 let circles;
 function CircleLayout(svg, topics, width, height, cx, cy, outer, i){
   if(outer){
@@ -71,19 +93,37 @@ function CircleLayout(svg, topics, width, height, cx, cy, outer, i){
         return cy - (R - r) * Math.cos(angle * i) * 19 / 20;
       })
       .attr('fill', '#B7B7B7');
-    for(let com in communityLinks){
-      if(communityLinks[com] !== undefined){
-        for(let endcom of communityLinks[com]){
-          let startIndex = processedCommunity.indexOf(com);
-          let endIndex = processedCommunity.indexOf(endcom);
+    for(let i = 0; i < circles._groups[0].length; i++){
+      let cx = parseFloat(circles._groups[0][i].attributes.cx.value);
+      let cy = parseFloat(circles._groups[0][i].attributes.cy.value);
+      svg.append('g')
+        .append('text')
+        .attr('x', cx)
+        .attr('y', cy)
+        .attr('font-size', '2em')
+        .attr('text-anchor', 'middle')
+        .attr('dy', '0.5em')
+        .text(topics[i]);
+    }
+    // for(let com in communityLinks){
+    //   if(communityLinks[com] !== undefined){
+    //     for(let endcom of communityLinks[com]){
+    for(let com in links0){
+      if(links0[com] !== undefined){
+        for(let endcom of links0[com]){
+          // let startIndex = processedCommunity.indexOf(com);
+          // let endIndex = processedCommunity.indexOf(endcom);
           // let startIndex = communities.indexOf(com);
           // let endIndex = communities.indexOf(endcom);
+          let startIndex = topics.indexOf(com);
+          let endIndex = topics.indexOf(endcom);
           let r = parseFloat(circles._groups[0][startIndex].attributes.r.value);
           let cx1 = parseFloat(circles._groups[0][startIndex].attributes.cx.value);
           let cy1 = parseFloat(circles._groups[0][startIndex].attributes.cy.value);
           let cx2 = parseFloat(circles._groups[0][endIndex].attributes.cx.value);
           let cy2 = parseFloat(circles._groups[0][endIndex].attributes.cy.value);
           let linkData = caculateLinkSourceTargetBetweenCircles(cx1,cy1,r,cx2,cy2,r);
+          
           // console.log(linkData)
           svg.append('path')
             .attr('d', link(linkData))
@@ -113,8 +153,9 @@ function CircleLayout(svg, topics, width, height, cx, cy, outer, i){
         return cy - (R - r) * Math.cos(angle * i) * 19 / 20;
       })
       .attr('fill', '#878787');
-    let topicS = processedTopics[(i+1).toString()];
-    // let topicS = topicsGroupByCommunityId[(i+1).toString()];
+    // let topicS = processedTopics[i];
+    // let linkS = linksInCommunity[i];
+    let topicS = topicsGroupByCommunityId[(i+1).toString()];
     let linkS = linksInCommunity[(i+1).toString()];
     for(let topic in linkS){
       if(linkS[topic] !== undefined){
@@ -152,16 +193,24 @@ function CircleLayout(svg, topics, width, height, cx, cy, outer, i){
   //                   .attr('fill', 'none');
 
 }
-// CircleLayout(svg, communities, width, height, width/2, height/2, true);
-CircleLayout(svg, processedCommunity, width, height, width/2, height/2, true);
+CircleLayout(svg, topics2, width, height, width/2, height/2, true);
+/*
+// 处理前
+CircleLayout(svg, communities, width, height, width/2, height/2, true);
+// 处理后
+// CircleLayout(svg, processedCommunity, width, height, width/2, height/2, true);
 for(let i = 0; i < processedCommunity.length; i++){
   // console.log(i)
   let cx = parseFloat(circles._groups[0][i].attributes.cx.value);
   let cy = parseFloat(circles._groups[0][i].attributes.cy.value);
   let r = parseFloat(circles._groups[0][i].attributes.r.value);
+  // 处理前
   CircleLayout(svg, topicsGroupByCommunityId[(i+1).toString()], r * 2, r * 2, cx, cy, false, i);
+  // 处理后
+  // CircleLayout(svg, topicsGroupByCommunityId[processedCommunity[i]], r * 2, r * 2, cx, cy, false, processedCommunity[i]);
   // console.log(circles._groups[0][i].attributes.cx.value)
 }
+*/
 // console.log(topicsGroupByCommunityId['7']);
 // topic1 = detectEdgeCrossing(topicsGroupByCommunityId['7'], linksInCommunity['7']);
 // console.log(topic1);
