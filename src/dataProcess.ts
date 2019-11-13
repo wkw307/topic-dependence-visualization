f5045277a399ceafa1b18b9d51a38166b0ec49bb
wkw307 <wkw307@gmail.com>
2019-11-13 20:49:19 +0800

    add ts config

---
+++ src/dataProcess.ts
@@ -0,0 +1,16 @@
+interface RelationRes {
+    startTopicId: number;
+    endTopicId: number;
+    startTopicName: string;
+    endTopicName: string;
+    domainId: number;
+}
+
+interface Topic {
+    topicName: string;
+    topicId: number;
+}
+
+function parseAPI(data: RelationRes[]) {
+
+}
