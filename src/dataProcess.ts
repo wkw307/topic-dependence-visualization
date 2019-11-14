6171f5021809ae13af72a2f23c6ea6f941e2205c
wkw307 <wkw307@gmail.com>
2019-11-14 17:31:05 +0800

    parse api

--- src/dataProcess.ts
+++ src/dataProcess.ts
@@ -1,3 +1,5 @@
+import { cloneDeep } from 'lodash';
+
 interface RelationRes {
     startTopicId: number;
     endTopicId: number;
@@ -6,11 +8,49 @@ interface RelationRes {
     domainId: number;
 }
 
-interface Topic {
-    topicName: string;
-    topicId: number;
+/**
+ * 解析api数据
+ * @param data
+ */
+function parseAPI(data: RelationRes[]): { topics: { [p: number]: string }, relations: { [p: number]: number[] }} {
+    const topics = {};
+    const relations = {};
+    for (let relation of data) {
+        topics[relation.startTopicId] = relation.startTopicName;
+        topics[relation.endTopicId] = relation.endTopicName;
+        if (relations[relation.startTopicId] === undefined) {
+            relations[relation.startTopicId] = [relation.endTopicId];
+        } else {
+            relations[relation.startTopicId].push(relation.endTopicId);
+        }
+    }
+    return { topics, relations };
 }
 
-function parseAPI(data: RelationRes[]) {
+/**
+ * 删除前向边，入度为零的点均挂载到虚拟节点上
+ */
+function preProcess(relations) {
+    const resultPaths = [];
+    // 找出所有路径
+    const paths: Array<Array<number>> = [];
+    for (let key in relations) {
+        if (relations.hasOwnProperty(key)) {
+            paths.push([parseInt(key)]);
+        }
+    }
+    while (paths.length > 0) {
+        const currPath = paths.shift();
+        const currTopic = currPath[currPath.length - 1];
+        if (relations[currTopic] === undefined) {
+            resultPaths.push(currPath);
+        } else {
+            for (let key of relations[currTopic]) {
+                const tmp = cloneDeep(currPath);
+                tmp.push(parseInt(key));
+                paths.push(tmp);
+            }
+        }
+    }
 
 }
