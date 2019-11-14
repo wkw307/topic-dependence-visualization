adce59b857106a1d023e4d3f87cbba86c93f2ae4
wkw307 <wkw307@gmail.com>
2019-11-14 21:26:09 +0800

    delete forward path; find entrance(1) nodes

--- src/dataProcess.ts
+++ src/dataProcess.ts
@@ -30,13 +30,15 @@ function parseAPI(data: RelationRes[]): { topics: { [p: number]: string }, relat
 /**
  * 删除前向边，入度为零的点均挂载到虚拟节点上
  */
-function preProcess(relations) {
+function preProcess(topics, relations) {
     const resultPaths = [];
+    const resultRelations = {};
     // 找出所有路径
     const paths: Array<Array<number>> = [];
     for (let key in relations) {
         if (relations.hasOwnProperty(key)) {
             paths.push([parseInt(key)]);
+            resultRelations[parseInt(key)] = [];
         }
     }
     while (paths.length > 0) {
@@ -52,5 +54,42 @@ function preProcess(relations) {
             }
         }
     }
-
+    // 找出前向边
+    const forwardEdge = [];
+    for (let key in relations) {
+        if (relations.hasOwnProperty(key)) {
+            for (let endTopicId of relations[key]) {
+                let flag = true;
+                for (let arr of resultPaths) {
+                    if (arr.indexOf(key) !== -1 && arr.indexOf(endTopicId) !== -1 && arr.indexOf(endTopicId) - arr.indexOf(key) > 1) {
+                        // key -> endTopicId 是前向边
+                        forwardEdge.push([key, endTopicId]);
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    resultRelations[key].push(endTopicId);
+                }
+            }
+        }
+    }
+    // 找出入度为0的节点
+    const entranceTopicIds = [];
+    for (let topicId in topics) {
+        if (topics.hasOwnProperty(topicId)) {
+            let flag = true;
+            for (let key in resultRelations) {
+                if (resultRelations[key].indexOf(topicId) !== -1) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                entranceTopicIds.push(topicId);
+            }
+        }
+    }
+    resultRelations[-1] = entranceTopicIds;
+    return resultRelations;
 }
