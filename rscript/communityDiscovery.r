ed5ca5d40de1f08291a168367e1ba82404a5043a
wkw307 <wkw307@gmail.com>
2019-11-21 16:27:09 +0800

    calc community

--- rscript/communityDiscovery.r
+++ rscript/communityDiscovery.r
@@ -19,9 +19,9 @@ ec_modularity <- modularity(ec)
 ec_com <- membership(ec)
 ec_com <- cbind(V(g),ec$membership) #V(g) gets the number of vertices
 ec_com <- cbind(V(g)$name,ec$membership) #To get names if your vertices are labeled
-write.table(ec_com, file=paste(output, 'ec.txt'))
+write.table(ec_com, file=paste(output, 'ec.txt', sep=''))
 print(paste('ec:', ec_modularity))
-jpeg(paste(output, 'EC.jpg'))
+jpeg(paste(output, 'EC.jpg', sep=''))
 plot(ec,directedG, layout=layout.auto, vertex.size=5,vertex.label=NA,edge.arrow.size=0.5,main=paste('EC',modularity(ec)))
 dev.off()
 #• Computing communities in large networks using random walks
@@ -31,9 +31,9 @@ wc_modularity <- modularity(wc)
 wc_com <- membership(wc)
 wc_com <- cbind(V(g),wc$membership) #V(g) gets the number of vertices
 wc_com <- cbind(V(g)$name,wc$membership) #To get names if your vertices are labeled
-write.table(wc_com, file=paste(output, 'wc.txt'))
+write.table(wc_com, file=paste(output, 'wc.txt', sep=''))
 print(paste('wc:', wc_modularity))
-jpeg(paste(output, 'WC.jpg'))
+jpeg(paste(output, 'WC.jpg', sep=''))
 plot(wc,directedG, layout=layout.auto, vertex.size=5,vertex.label=NA,edge.arrow.size=0.5,main=paste('WC',modularity(wc)))
 dev.off()
 #• Finding community structure in networks using the eigenvectors of matrices
@@ -43,9 +43,9 @@ lec_modularity <- modularity(lec)
 lec_com <- membership(lec)
 lec_com <- cbind(V(g),lec$membership) #V(g) gets the number of vertices
 lec_com <- cbind(V(g)$name,lec$membership) #To get names if your vertices are labeled
-write.table(lec_com, file=paste(output, 'lec.txt'))
+write.table(lec_com, file=paste(output, 'lec.txt', sep=''))
 print(paste('lec:',lec_modularity))
-jpeg(paste(output, 'LEC.jpg'))
+jpeg(paste(output, 'LEC.jpg', sep=''))
 plot(lec,directedG, layout=layout.auto, vertex.size=5,vertex.label=NA,edge.arrow.size=0.5,main=paste('LEC',modularity(lec)))
 dev.off()
 #• Finding community structure in very large networks
@@ -57,9 +57,9 @@ fc_modularity <- modularity(fc)
 fc_com <- membership(fc)
 fc_com <- cbind(V(g),fc$membership) #V(g) gets the number of vertices
 fc_com <- cbind(V(g)$name,fc$membership) #To get names if your vertices are labeled
-write.table(fc_com, file=paste(output, 'fc.txt'))
+write.table(fc_com, file=paste(output, 'fc.txt', sep=''))
 print(paste('fc:', fc_modularity))
-jpeg(paste(output, 'FC.jpg'))
+jpeg(paste(output, 'FC.jpg', sep=''))
 plot(fc,directedG, layout=layout.auto, vertex.size=5,vertex.label=NA,edge.arrow.size=0.5,main=paste('FC',modularity(fc)))
 dev.off()
 #• Fast unfolding of communities in large networks
@@ -69,9 +69,9 @@ mc_modularity <- modularity(mc)
 mc_com <- membership(mc)
 mc_com <- cbind(V(g),mc$membership) #V(g) gets the number of vertices
 mc_com <- cbind(V(g)$name,mc$membership) #To get names if your vertices are labeled
-write.table(mc_com, file=paste(output, 'mc.txt'))
+write.table(mc_com, file=paste(output, 'mc.txt', sep=''))
 print(paste('mc:',mc_modularity))
-jpeg(paste(output, 'MC.jpg'))
+jpeg(paste(output, 'MC.jpg', sep=''))
 plot(mc,directedG, layout=layout.auto, vertex.size=5,vertex.label=NA,edge.arrow.size=0.5,main=paste('MC',modularity(mc)))
 dev.off()
 #• Near linear time algorithm to detect community structures in large-scale networks.
@@ -82,9 +82,9 @@ lc_modularity <- modularity(lc)
 lc_com <- membership(lc)
 lc_com <- cbind(V(g),lc$membership) #V(g) gets the number of vertices
 lc_com <- cbind(V(g)$name,lc$membership) #To get names if your vertices are labeled
-write.table(lc_com, file=paste(output, 'lc.txt'))
+write.table(lc_com, file=paste(output, 'lc.txt', sep=''))
 print(paste('lc:', lc_modularity))
-jpeg(paste(output, 'LC.jpg'))
+jpeg(paste(output, 'LC.jpg', sep=''))
 plot(lc,directedG, layout=layout.auto, vertex.size=5,vertex.label=NA,edge.arrow.size=0.5,main=paste('LC',modularity(lc)))
 dev.off()
 arr <- c(ec_modularity, wc_modularity, lec_modularity, fc_modularity, mc_modularity, lc_modularity)
